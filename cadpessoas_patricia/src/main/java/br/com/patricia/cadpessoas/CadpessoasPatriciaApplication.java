6e33474a42b3adddb8bd28046e69ef13d438a7f0
PatriciaNayara <patricia.santos@alunos.ufersa.edu.br>
2021-04-12 17:35:58 -0300

    Iniciando CRUD da unidade 1

---
+++ cadpessoas_patricia/src/main/java/br/com/patricia/cadpessoas/CadpessoasPatriciaApplication.java
@@ -0,0 +1,13 @@
+package br.com.patricia.cadpessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoasPatriciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoasPatriciaApplication.class, args);
+	}
+
+}
---
+++ cadpessoas_patricia/src/test/java/br/com/patricia/cadpessoas/CadpessoasPatriciaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.patricia.cadpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadpessoasPatriciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
