1bcfd0115b22a8911bfad56c46a22cde99f5adee
PatriciaNayara <patricia.santos@alunos.ufersa.edu.br>
2021-04-13 00:07:29 -0300

    Atualização

--- cadpessoas_patricia/src/main/java/br/com/patricia/cadpessoas/controllers/PessoaController.java
+++ cadpessoas_patricia/src/main/java/br/com/patricia/cadpessoas/controllers/PessoaController.java
@@ -1,10 +1,14 @@
 package br.com.patricia.cadpessoas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import br.com.patricia.cadpessoas.model.Pessoa;
 import br.com.patricia.cadpessoas.repositories.PessoaRepository;
 
 @Controller
@@ -18,6 +22,14 @@ public class PessoaController {
 	public String Index() {
 		return "index.html";
 		
+	}
+	
+	@GetMapping("/listarPessoas")
+	public ModelAndView listarPessoas() {
+			List<Pessoa> lista = pessoaRepo.findAll();
+			ModelAndView mav = new ModelAndView("listarPessoas");
+			mav.addObject ("pessoas", lista);
+			return mav;
 	}
 	
 	
